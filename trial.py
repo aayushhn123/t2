f4ba9caa5743fd8ea475d8fda59093b8492c37c3
Aayush Nayak <88595661+aayushhn123@users.noreply.github.com>
2025-07-29 22:34:21 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -1052,7 +1052,7 @@ def generate_pdf_timetable(semester_wise_timetable, output_pdf):
     try:
         reader = PdfReader(output_pdf)
         writer = PdfWriter()
-        page_number_pattern = re.compile(r'^[\s\n]*(?:Page\s*)?\d+[\s\n]*)
+        page_number_pattern = re.compile(r'^[\s\n]*(?:Page\s*)?\d+[\s\n]*$')
         for page_num in range(len(reader.pages)):
             page = reader.pages[page_num]
             try:
